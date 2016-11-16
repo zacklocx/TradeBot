676af59aa85273cf9f006ff5859dd07d6aa33a8f
zacklocx <zacklocx@gmail.com>
2016-11-16 19:32:07 +0800

    add first version files

---
+++ src/TradeBot.cpp
@@ -0,0 +1,13 @@
+
+#include "TradeBotConfig.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "TradeBot Start" << endl;
+
+	return 0;
+}
---
+++ src/TradeBotConfig.h
@@ -0,0 +1,4 @@
+#define TB_VERSION_MAJOR 1
+#define TB_VERSION_MINOR 0
+
+#define TB_DEBUG
