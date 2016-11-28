d35c03fcb3609d50f3f50bf507ddd57f70f19645
zacklocx <zacklocx@gmail.com>
2016-11-28 23:36:03 +0800

    update api

--- src/api.cpp
+++ src/api.cpp
@@ -3,82 +3,52 @@
 
 #include <sstream>
 
-#include "utils.h"
-
-std::string api_t::prepare() const
+param_type api_t::prepare() const
 {
 	static std::string api_key = "91a440cb-d0c2-4dd6-924e-320d2a95a543";
 	static std::string secret_key = "8AF27D70C7D51568ADC74FA0CBF707F0";
+
+	if("GET" == method_)
+	{
+		return param_;
+	}
 
 	std::ostringstream param_stream;
 
 	std::string separator = "";
 
-	if("POST" == method_)
-	{
-		param_stream << "api_key=" << api_key << "&";
-	}
+	param_stream << "api_key=" << api_key << "&";
 
 	for(const auto& it : param_)
 	{
-		param_stream << separator << it.first << "=" << urlencode(it.second);
+		param_stream << separator << it.first << "=" << it.second;
 		separator = "&";
 	}
 
-	if("POST" == method_)
-	{
-		std::ostringstream param_with_key;
-		param_with_key << param_stream.str() << separator << "secret_key=" << secret_key;
+	param_stream << separator << "secret_key=" << secret_key;
 
-		std::string sign = md5(param_with_key.str());
+	std::string sign = md5(param_stream.str());
 
-		param_stream << separator << "sign=" << sign;
-	}
+	param_type ret(param_);
 
-	std::ostringstream request_stream;
+	param_type["api_key"] = api_key;
+	param_type["sign"] = sign;
 
-	if("GET" == method_)
-	{
-		request_stream << method_ << " " << interface_ << "?"
-			<< param_stream.str() << " HTTP/1.1\r\n";
-	}
-	else
-	{
-		request_stream << method_ << " " << interface_ << " HTTP/1.1\r\n";
-	}
-
-	request_stream << "Host: " << host_ << "\r\n";
-	request_stream << "Accept: */*\r\n";
-
-	if("POST" == method_)
-	{
-		request_stream << "Content-Length: " << param_stream.str().length() << "\r\n";
-	}
-
-	request_stream << "Content-Type: application/x-www-form-urlencoded" << "\r\n";
-	request_stream << "Connection: close\r\n\r\n";
-
-	if("POST" == method_)
-	{
-		request_stream << param_stream.str();
-	}
-
-	return request_stream.str();
+	return ret;
 }
 
 std::ostream& operator<<(std::ostream& out, const api_t& api)
 {
-	out << "host: " << api.host() << "\n";
-	out << "interface: " << api.interface() << "\n";
+	out << "url: " << api.url() << "\n";
 	out << "method: " << api.method() << "\n";
 
-	out << "param:" << "\n";
+	out << "[param]";
 
 	param_type param = api.param();
 
 	for(const auto& it : param)
 	{
-		out << "  " << it.first << ": " << it.second << "\n";
+		out << "\n" << it.first << ": " << it.second;
 	}
 
 	return out;
--- src/api.h
+++ src/api.h
@@ -11,28 +11,22 @@ typedef std::map<std::string, std::string> param_type;
 class api_t
 {
 public:
-	api_t(const std::string& host = "", const std::string& interface = "",
-		const std::string& method = "", const param_type& param = param_type()) :
-		host_(host), interface_(interface), method_(method), param_(param)
+	api_t(const std::string& url = "", const std::string& method = "", const param_type& param = param_type()) :
+		url_(url), method_(method), param_(param)
 	{}
 
-	std::string host() const { return host_; }
-	std::string interface() const { return interface_; }
+	std::string url() const { return url_; }
 	std::string method() const { return method_; }
 
 	param_type param() const { return param_; }
 
-	void update_host(const std::string& host) { host_ = host; }
-	void update_interface(const std::string& interface) { interface_ = interface; }
-	void update_method(const std::string& method) { method_ = method; }
-
 	void update_param(const param_type& param) { param_ = param; }
 	void update_param(const std::string& key, const std::string& value) { param_[key] = value; }
 
-	std::string prepare() const;
+	param_type prepare() const;
 
 private:
-	std::string host_, interface_, method_;
+	std::string url_, method_;
 	param_type param_;
 };
 
