009809b6ce053e01dff4e1f005b7866227f5ab12
zacklocx <zacklocx@gmail.com>
2016-12-02 00:30:27 +0800

    refine client code

--- src/client.cpp
+++ src/client.cpp
@@ -1,13 +1,14 @@
 
 #include "client.h"
 
-#include <string>
-#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <exception>
 
-#include "dump.h"
-
-void client_t::call(const api_t& api, boost::system::error_code& ec, Json::Value& data)
+void client_t::call(const api_t& api, boost::system::error_code& ec, Json::Value& json)
 {
+	data_ = "";
+
 	std::string method = api.method();
 
 	if("GET" == method)
@@ -22,25 +23,98 @@ void client_t::call(const api_t& api, boost::system::error_code& ec, Json::Value
 		avhttp::post_form(stream_, api.prepare());
 		stream_.open(api.url(), ec);
 	}
-
-	if(!ec)
-	{
-		std::istream in(&stream_);
-
-		std::string s, body;
-
-		while(in >> s)
-		{
-			body.append(s);
-		}
-
-		Json::Reader reader;
-		reader.parse(body, data);
-	}
 	else
 	{
-		LLOG() << "error: " << ec.message();
+		throw std::logic_error("bad method");
+	}
+
+	while(!ec)
+	{
+		std::size_t bytes_transferred = stream_.read_some(boost::asio::buffer(buffer_), ec);
+		std::copy(buffer_.begin(), buffer_.begin() + bytes_transferred, std::back_inserter(data_));
 	}
 
 	stream_.close();
+
+	if(boost::asio::error::eof == ec)
+	{
+		Json::Reader reader;
+		reader.parse(data_, json);
+	}
+}
+
+void client_t::async_call(const api_t& api, handler_type handler)
+{
+	handler_ = handler;
+
+	data_ = "";
+
+	std::string method = api.method();
+
+	if("GET" == method)
+	{
+		std::string query = avhttp::map_to_query(api.param());
+		std::string full_url = api.url() + "?" + query;
+
+		stream_.async_open(full_url, boost::bind(&client_t::handle_open, this, boost::asio::placeholders::error));
+	}
+	else if("POST" == method)
+	{
+		avhttp::post_form(stream_, api.prepare());
+		stream_.async_open(api.url(), boost::bind(&client_t::handle_open, this, boost::asio::placeholders::error));
+	}
+	else
+	{
+		throw std::logic_error("bad method");
+	}
+}
+
+void client_t::handle_open(const boost::system::error_code& ec)
+{
+	if(!ec)
+	{
+		stream_.async_read_some(boost::asio::buffer(buffer_),
+			boost::bind(&client_t::handle_read, this,
+				boost::asio::placeholders::bytes_transferred,
+				boost::asio::placeholders::error));
+	}
+	else
+	{
+		stream_.close();
+
+		if(handler_)
+		{
+			handler_(ec, Json::Value());
+		}
+	}
+}
+
+void client_t::handle_read(int bytes_transferred, const boost::system::error_code& ec)
+{
+	if(!ec)
+	{
+		std::copy(buffer_.begin(), buffer_.begin() + bytes_transferred, std::back_inserter(data_));
+
+		stream_.async_read_some(boost::asio::buffer(buffer_),
+			boost::bind(&client_t::handle_read, this,
+				boost::asio::placeholders::bytes_transferred,
+				boost::asio::placeholders::error));
+	}
+	else
+	{
+		stream_.close();
+
+		if(handler_)
+		{
+			Json::Value json;
+
+			if(boost::asio::error::eof == ec)
+			{
+				Json::Reader reader;
+				reader.parse(data_, json);
+			}
+
+			handler_(ec, json);
+		}
+	}
 }
--- src/client.h
+++ src/client.h
@@ -2,6 +2,9 @@
 #ifndef CLIENT_INCLUDED
 #define CLIENT_INCLUDED
 
+#include <string>
+#include <functional>
+
 #include <json/json.h>
 
 #include "avhttp.hpp"
@@ -11,12 +14,23 @@
 class client_t
 {
 public:
+	typedef std::function<void(const boost::system::error_code&, const Json::Value&)> handler_type;
+
 	client_t(boost::asio::io_service& service) : stream_(service) {}
 
-	void call(const api_t& api, boost::system::error_code& ec, Json::Value& data);
+	void call(const api_t& api, boost::system::error_code& ec, Json::Value& json);
+	void async_call(const api_t& api, handler_type handler);
 
 private:
 	avhttp::http_stream stream_;
+
+	handler_type handler_;
+
+	std::string data_;
+	boost::array<char, 1024> buffer_;
+
+	void handle_open(const boost::system::error_code& ec);
+	void handle_read(int bytes_transferred, const boost::system::error_code& ec);
 };
 
 #endif /* CLIENT_INCLUDED */
