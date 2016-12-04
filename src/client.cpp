f52ca628c670284ce3089aa486427640f1df2832
zacklocx <zacklocx@gmail.com>
2016-12-04 17:41:38 +0800

    make client class copyable

--- src/client.cpp
+++ src/client.cpp
@@ -22,12 +22,12 @@ bool client_t::call(const api_t& api, Json::Value& json)
 		std::string query = avhttp::map_to_query(api.param());
 		std::string full_url = api.url() + "?" + query;
 
-		stream_.open(full_url, ec);
+		stream_->open(full_url, ec);
 	}
 	else if("POST" == method)
 	{
-		avhttp::post_form(stream_, api.prepare());
-		stream_.open(api.url(), ec);
+		avhttp::post_form(*stream_, api.prepare());
+		stream_->open(api.url(), ec);
 	}
 	else
 	{
@@ -36,11 +36,11 @@ bool client_t::call(const api_t& api, Json::Value& json)
 
 	while(!ec)
 	{
-		std::size_t bytes_transferred = stream_.read_some(boost::asio::buffer(buffer_), ec);
-		std::copy(buffer_.begin(), buffer_.begin() + bytes_transferred, std::back_inserter(data_));
+		std::size_t bytes_transferred = stream_->read_some(boost::asio::buffer(*buffer_), ec);
+		std::copy(buffer_->begin(), buffer_->begin() + bytes_transferred, std::back_inserter(data_));
 	}
 
-	stream_.close();
+	stream_->close();
 
 	if(boost::asio::error::eof == ec ||
 		(boost::asio::error::get_ssl_category() == ec.category() &&
@@ -72,12 +72,12 @@ void client_t::async_call(const api_t& api, handler_type handler)
 		std::string query = avhttp::map_to_query(api.param());
 		std::string full_url = api.url() + "?" + query;
 
-		stream_.async_open(full_url, boost::bind(&client_t::handle_open, this, boost::asio::placeholders::error));
+		stream_->async_open(full_url, boost::bind(&client_t::handle_open, this, boost::asio::placeholders::error));
 	}
 	else if("POST" == method)
 	{
-		avhttp::post_form(stream_, api.prepare());
-		stream_.async_open(api.url(), boost::bind(&client_t::handle_open, this, boost::asio::placeholders::error));
+		avhttp::post_form(*stream_, api.prepare());
+		stream_->async_open(api.url(), boost::bind(&client_t::handle_open, this, boost::asio::placeholders::error));
 	}
 	else
 	{
@@ -89,14 +89,14 @@ void client_t::handle_open(const boost::system::error_code& ec)
 {
 	if(!ec)
 	{
-		stream_.async_read_some(boost::asio::buffer(buffer_),
+		stream_->async_read_some(boost::asio::buffer(*buffer_),
 			boost::bind(&client_t::handle_read, this,
 				boost::asio::placeholders::bytes_transferred,
 				boost::asio::placeholders::error));
 	}
 	else
 	{
-		stream_.close();
+		stream_->close();
 
 		LLOG() << "client error: " << ec.message();
 
@@ -111,16 +111,16 @@ void client_t::handle_read(int bytes_transferred, const boost::system::error_cod
 {
 	if(!ec)
 	{
-		std::copy(buffer_.begin(), buffer_.begin() + bytes_transferred, std::back_inserter(data_));
+		std::copy(buffer_->begin(), buffer_->begin() + bytes_transferred, std::back_inserter(data_));
 
-		stream_.async_read_some(boost::asio::buffer(buffer_),
+		stream_->async_read_some(boost::asio::buffer(*buffer_),
 			boost::bind(&client_t::handle_read, this,
 				boost::asio::placeholders::bytes_transferred,
 				boost::asio::placeholders::error));
 	}
 	else
 	{
-		stream_.close();
+		stream_->close();
 
 		if(handler_)
 		{
--- src/client.h
+++ src/client.h
@@ -2,7 +2,9 @@
 #ifndef CLIENT_INCLUDED
 #define CLIENT_INCLUDED
 
+#include <array>
 #include <string>
+#include <memory>
 #include <functional>
 
 #include <json/json.h>
@@ -16,18 +18,23 @@ class client_t
 public:
 	typedef std::function<void(bool, const Json::Value&)> handler_type;
 
-	client_t(boost::asio::io_service& service) : stream_(service) {}
+	client_t(boost::asio::io_service& service) : stream_(std::make_shared<avhttp::http_stream>(service))
+	{
+		buffer_ = std::make_shared<std::array<char, BUFFER_SIZE>>();
+	}
 
 	bool call(const api_t& api, Json::Value& json);
 	void async_call(const api_t& api, handler_type handler);
 
 private:
-	avhttp::http_stream stream_;
+	std::shared_ptr<avhttp::http_stream> stream_;
 
 	handler_type handler_;
 
 	std::string data_;
-	boost::array<char, 1024> buffer_;
+	std::shared_ptr<std::array<char, BUFFER_SIZE>> buffer_;
+
+	static const int BUFFER_SIZE = 1024;
 
 	void handle_open(const boost::system::error_code& ec);
 	void handle_read(int bytes_transferred, const boost::system::error_code& ec);
